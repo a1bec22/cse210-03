daa90227eca9bfc345a07fa5bd58f2758c6cd51b
jessica14angulo <ang21016@byui.edu>
2022-02-04 13:37:28 -0400

    player class

---
+++ player.py
@@ -0,0 +1,10 @@
+class Player():
+    def __init__(self):
+        self.chosen_letters = []
+
+    def guess_letter(self):
+        guess = str(input("Guess your letter a-z! ")).lower()
+        while len(guess) != 1 or guess in self.chosen_letters:
+            guess = str(input("Guess your letter a-z! "))
+        self.chosen_letters.append(guess.lower())  
+        return guess
